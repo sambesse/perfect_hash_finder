3f04c804f0afc527e6cc901586ecb81584c23a65
Sam Besse <sam.besse@gmail.com>
2025-08-24 21:38:07 -0400

    making the number of can ids a cli input and randomizing them. Works so far, great for benchmarking the different approaches

--- perfect_hash_finder.c
+++ perfect_hash_finder.c
@@ -4,99 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <time.h>
+#include <errno.h>
 
 #include "uthash.h"
-
-const uint16_t can_ids [] = {
-    0x110,
-    0x600,
-    0x601,
-    0x602,
-    0x40,
-    0xA,
-    0x5A,
-    0x16,
-    0x297,
-    0x313,
-    0x300,
-    0x306,
-    0x123,
-    0x124,
-    0x125,
-    0x129,
-    0x22,
-    0x29,
-    0x30,
-    0x12,
-    0x19,
-    0x200,
-    0x201,
-    0x202,
-    0x203,
-    0x204,
-    0x205,
-    0x206,
-    0x207,
-    0x209,
-    0x210,
-    0x211,
-    0x212,
-    0x213,
-    0x214,
-    0x215,
-    0x216,
-    0x217,
-    0x218,
-    0x219,
-    0x220,
-};
-
-char *tag[] = {
-    "110",
-    "600",
-    "601",
-    "602",
-    "40",
-    "A",
-    "5A",
-    "16",
-    "297",
-    "313",
-    "300",
-    "306",
-    "123",
-    "124",
-    "125",
-    "129",
-    "22",
-    "29",
-    "30",
-    "12",
-    "19",
-    "200",
-    "201",
-    "202",
-    "203",
-    "204",
-    "205",
-    "206",
-    "207",
-    "208",
-    "209",
-    "210",
-    "211",
-    "212",
-    "213",
-    "214",
-    "215",
-    "216",
-    "217",
-    "218",
-    "219",
-    "220",
-
-};
 
 struct key_value {
     uint16_t key;
@@ -108,8 +20,8 @@ int compare(const void *a, const void *b) {
     return ((struct key_value *)a)->key - ((struct key_value *)b)->key;
 }
 
-#define NUM_INPUTS (uint32_t)(sizeof(can_ids) / (sizeof(can_ids[0])))
 #define N_STRESS_TEST   100000
+#define MAX_CAN_ID      0x610
 
 #define HASH(x) ((k*x % p) % m)
 
@@ -118,8 +30,9 @@ const uint32_t max_k = (UINT32_MAX / 4096);
 const uint32_t min_k = (uint32_t)(p / 10); //smallest CAN id, so we don't search things where the x%p=x
 struct key_value **hash_table;
 uint32_t k = min_k;
+uint32_t num_inputs = 0;
 
-uint32_t m = NUM_INPUTS;
+uint32_t m = 0;
 
 struct key_value *find_hash(uint16_t key);
 struct key_value *inputs;
@@ -129,31 +42,67 @@ struct key_value *lib_find_hash(uint16_t key);
 
 struct key_value *lib_hash = NULL;
 
-int main() {
-    inputs = malloc(NUM_INPUTS * sizeof(struct key_value));
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("too few arguments, usage: perfect_hash_finder <num_elems>");
+        return -1;
+    }
+    num_inputs = atoi(argv[1]);
+    if (num_inputs < 0 || (num_inputs == 0 && errno != 0)) {
+        perror("failed to convert number of arguments");
+        return -1;
+    }
+    m = num_inputs;
+    printf("starting with %d inputs, %d buckets\n", num_inputs, m);
+
+    uint16_t *can_ids = malloc(num_inputs * sizeof(uint16_t));
+
+    for (uint32_t i = 0; i < num_inputs; i++) {
+        can_ids[i] = rand() % MAX_CAN_ID;
+        printf("assigning %X to index %d\n", can_ids[i], i);
+        uint32_t j = 0;
+        for (; j < i; j++) {
+            if (can_ids[j] == can_ids[i]) {
+                break;
+            }
+        }
+        if (j < i) {
+            printf("found duplicate value, trying again\n");
+            i--;
+            continue;
+        }
+    }
+
+    inputs = malloc(num_inputs * sizeof(struct key_value));
     if (!inputs) {
-        printf("failed to malloc inputs");
+        perror("failed to malloc inputs");
         return -1;
     }
 
-    for (uint32_t i = 0; i < NUM_INPUTS; i++) {
+    for (uint32_t i = 0; i < num_inputs; i++) {
         inputs[i].key = can_ids[i];
-        inputs[i].val = tag[i];
+        inputs[i].val = malloc(16);
+        if(inputs[i].val)
+            sprintf(inputs[i].val, "%x", can_ids[i]);
+        else {
+            perror("failed to malloc");
+            return -1;
+        }
         HASH_ADD(hh, lib_hash, key, 2, &inputs[i]);
     }
 
-    qsort(inputs, NUM_INPUTS, sizeof(inputs[0]), compare);
+    qsort(inputs, num_inputs, sizeof(inputs[0]), compare);
 
     hash_table = malloc(m * sizeof(struct key_value *));
     if (!hash_table) {
-        printf("failed to allocate hash table\n");
+        perror("failed to allocate hash table");
         return -1;
     }
-    printf("find a k value with num_inputs: %d, num_buckets: %d, starting k: %d, max k: %d\n", NUM_INPUTS, m, min_k, max_k);
+    printf("find a k value with num_inputs: %d, num_buckets: %d, starting k: %d, max k: %d\n", num_inputs, m, min_k, max_k);
     memset(hash_table, 0, m * sizeof(struct key_value *));
     uint32_t i = 0;
     while(1) {
-        for(; i < NUM_INPUTS; i++) {
+        for(; i < num_inputs; i++) {
             uint32_t index = HASH(inputs[i].key);
             if (hash_table[index] != 0) {
                 break;
@@ -161,7 +110,7 @@ int main() {
                 hash_table[index] = &inputs[i];
             }
         }
-        if (i == NUM_INPUTS) { //congrats no collisions
+        if (i == num_inputs) { //congrats no collisions
             break;
         }
         i = 0;
@@ -175,7 +124,7 @@ int main() {
             free(hash_table);
             hash_table = malloc(m * sizeof(struct key_value *));
             if (!hash_table) {
-                printf("failed to allocate hash table\n");
+                perror("failed to allocate hash table\n");
                 return -1;
             }
             memset(hash_table, 0, m * sizeof(struct key_value *));
@@ -189,7 +138,7 @@ int main() {
             printf("%X\n", 0);
     }
 
-    for (uint32_t i = 0; i < NUM_INPUTS; i++) {
+    for (uint32_t i = 0; i < num_inputs; i++) {
         struct key_value *ret;
         printf("i = %d\n", i);
         printf("testing value %x\n", inputs[i].key);
@@ -221,7 +170,7 @@ void stress_test_hash(void) {
         total_time_hash += (end.tv_sec - start.tv_sec)*1e9 + (end.tv_nsec - start.tv_nsec);
 
         clock_gettime(CLOCK_MONOTONIC, &start);
-        res = bsearch(&key_struct, inputs, NUM_INPUTS, sizeof(inputs[0]), compare);
+        res = bsearch(&key_struct, inputs, num_inputs, sizeof(inputs[0]), compare);
         clock_gettime(CLOCK_MONOTONIC, &end);
         total_time_bsearch += (end.tv_sec - start.tv_sec)*1e9 + (end.tv_nsec - start.tv_nsec);
 
@@ -242,7 +191,7 @@ void stress_test_hash(void) {
 }
 
 int dumb_search(uint16_t key) {
-    for (uint32_t i = 0; i < NUM_INPUTS; i++) {
+    for (uint32_t i = 0; i < num_inputs; i++) {
         if (inputs[i].key == key)
             return i;
     }
